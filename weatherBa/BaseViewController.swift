26122eb4e3ad338f79dfd5493b30858045be42e9
Marcelo Nardone <marcelonardone@hotmail.com>
2018-10-27 10:35:07 -0300

    Reorden de Home y Agregamos BaseViewController

---
+++ weatherBa/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  weatherBa
+//
+//  Created by Marcelo Nardone on 27/10/2018.
+//  Copyright © 2018 Marcelo Nardone. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
